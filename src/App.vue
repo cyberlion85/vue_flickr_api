e37f12486bab2cb7fa7a4d1f96ed7578df74b95e
Stanislav Persanov <s.persanov@gmail.com>
2020-09-18 21:59:41 +0300

    add axios, basw outpu with photos

--- src/App.vue
+++ src/App.vue
@@ -11,48 +11,60 @@
     </form>
     <p v-if="loading">Loading...</p>
     <ul v-else>
-      <li v-for="image in images" :key="image.id">{{ image }}</li>
+      <li v-for="image in images" :key="image.id">
+  <img :src="image.url_n" :alt="image.title">
+  <div>
+    <p v-if="image.title">{{image.title}}</p>
+    <p v-else>No Title Found</p>
+    <p>By {{image.ownername}}</p>
+    <section>
+      <p>{{image.datetaken}}</p>
+      <p>Views: {{image.views}}</p>
+    </section>
+  </div>
+</li>
     </ul>
   </div>
 </template>
 
 <script>
-import axios from "axios";
-import config from "../../config.js";
+import axios from 'axios';
+import config from '../config';
 
 export default {
-  name: "home",
+  name: 'home',
   data() {
     return {
       loading: false,
-      tag: "",
-      images: []
+      tag: '',
+      images: [],
     };
   },
   methods: {
     search() {
       this.loading = true;
-      this.fetchImages().then(response => {
+      this.fetchImages().then((response) => {
         this.images = response.data.photos.photo;
+        // console.log(this.images.id);
         this.loading = false;
       });
     },
     fetchImages() {
       return axios({
-        method: "get",
-        url: "https://api.flickr.com/services/rest",
+        method: 'get',
+        url: 'https://api.flickr.com/services/rest',
         params: {
-          method: "flickr.photos.search",
+          method: 'flickr.photos.search',
           api_key: config.api_key,
           tags: this.tag,
-          extras: "url_n, owner_name, date_taken, views",
+          extras: 'url_n, owner_name, date_taken, views',
           page: 1,
-          format: "json",
+          format: 'json',
           nojsoncallback: 1,
-          per_page: 30
-        }
+          per_page: 30,
+        },
       });
-    }
-  }
+    },
+  },
 };
 </script>
